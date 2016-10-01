911d58b05bdf2fa3d87acca914aa92e2b70db0dd
Henry McCallaghan <henrymcc@gmail.com>
2016-10-01 20:54:50 +0200

    added a new pokemon class

---
+++ Pokedex-By-HmCc/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex-By-HmCc
+//
+//  Created by david johnson on 2016/10/01.
+//  Copyright © 2016 hiddenplatform. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+       return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
