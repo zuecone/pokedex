fc9372c89582937cda01218f0e4f4f5bc4101b41
Henry McCallaghan <henrymcc@gmail.com>
2016-10-09 12:07:33 +0200

    Added in the IBOutlest for the description view.  Changed the Pokemon class to include these fields as well

--- Pokedex-By-HmCc/PokemonDetailVC.swift
+++ Pokedex-By-HmCc/PokemonDetailVC.swift
@@ -9,14 +9,30 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+   
+    @IBOutlet weak var pokeLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenceLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     var pokemon: Pokemon!
     
-    @IBOutlet weak var pokeLabel: UILabel!
     
     override func viewDidLoad() {
         pokeLabel.text = pokemon.name.capitalizedString
     }
     
+    @IBAction func backButtonPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
 }
